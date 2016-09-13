c474c2a7a4698fd4d6e75e6e9618eff7567361ff
Remo H. Jansen <remo.jansen@wolksoftware.com>
2016-09-13 21:52:39 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import { InversifyExpressServer } from "./server";
 import { Controller, Method, Get, Put, Post, Patch, Head, All, Delete } from "./decorators";
 import { TYPE } from "./constants";
+import interfaces from "./interfaces";
 
 export {
+    interfaces,
     InversifyExpressServer,
     Controller,
     Method,
