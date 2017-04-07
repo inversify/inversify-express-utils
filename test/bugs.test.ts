585eb3bedeae514f9ee56da476144eb1448630a4
remojansen <remo.jansen@wolksoftware.com>
2017-04-07 02:02:28 +0100

    Added test case for inversify/InversifyJS/issues/527

---
+++ test/bugs.test.ts
@@ -0,0 +1,70 @@
+import { expect } from "chai";
+import * as express from "express";
+import { Controller, Method, Get, Request, Response, RequestParam } from "../src/decorators";
+import { interfaces } from "../src/interfaces";
+import { METADATA_KEY, PARAMETER_TYPE } from "../src/constants";
+import { InversifyExpressServer } from "../src/server";
+import { Container, injectable } from "inversify";
+import { TYPE } from "../src/constants";
+import * as request from "supertest";
+
+describe("Unit Test: Previous bugs", () => {
+
+    it("should support multiple controller methods with param annotations", (done) => {
+
+        let container = new Container();
+
+        @injectable()
+        @Controller("/api/test")
+        class TestController {
+            @Get("/")
+            public get(
+                @Request() req: express.Request,
+                @Response() res: express.Response
+            ) {
+                expect(req.url).not.to.eql(undefined);
+                expect((req as any).setHeader).to.eql(undefined);
+                expect(res.setHeader).not.to.eql(undefined);
+                expect((res as any).url).to.eql(undefined);
+                res.json([{ id: 1 }, { id: 2 }]);
+            }
+            @Get("/:id")
+            public getById(
+                @RequestParam("id") id: string,
+                @Request() req: express.Request,
+                @Response() res: express.Response
+            ) {
+                expect(id).to.eql("5");
+                expect(req.url).not.to.eql(undefined);
+                expect((req as any).setHeader).to.eql(undefined);
+                expect(res.setHeader).not.to.eql(undefined);
+                expect((res as any).url).to.eql(undefined);
+                res.json({ id: id });
+            }
+        }
+
+        container.bind(TYPE.Controller).to(TestController);
+        let server = new InversifyExpressServer(container);
+        let app = server.build();
+
+        request(app).get("/api/test/")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .then(response1 => {
+                        expect(Array.isArray(response1.body)).to.eql(true);
+                        expect(response1.body[0].id).to.eql("1");
+                        expect(response1.body[0].id).to.eql("2");
+                    });
+
+        request(app).get("/api/test/5")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .then(response2 => {
+                        expect(Array.isArray(response2.body)).to.eql(false);
+                        expect(response2.body.id).to.eql("5");
+                        done();
+                    });
+
+    });
+
+});
