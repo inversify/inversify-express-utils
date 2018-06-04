e09b2523a46a3488ceec65f9bb3624b2e623886d
jan-molak <jan.molak@gmail.com>
2018-06-04 17:30:48 +0100

    Middleware is capable of (re)binding Inversify services in a scope of an Express HTTP request. Closes inversify/inversifyjs#381

--- src/base_middleware.ts
+++ src/base_middleware.ts
@@ -1,5 +1,5 @@
 import * as express from "express";
-import { injectable } from "inversify";
+import { Container, injectable, interfaces as inversifyInterfaces } from "inversify";
 import { injectHttpContext } from "./decorators";
 import { interfaces } from "./interfaces";
 
@@ -8,6 +8,15 @@ export abstract class BaseMiddleware implements BaseMiddleware {
     // httpContext is initialized when the middleware is invoked
     // see resolveMidleware in server.ts for more details
     protected readonly httpContext: interfaces.HttpContext;
+
+    private readonly _container: Container;
+
+    protected bind<T>(serviceIdentifier: inversifyInterfaces.ServiceIdentifier<T>): inversifyInterfaces.BindingToSyntax<T> {
+        return this._container.isBound(serviceIdentifier)
+            ? this._container.rebind(serviceIdentifier)
+            : this._container.bind(serviceIdentifier);
+    }
+
     public abstract handler(
         req: express.Request,
         res: express.Response,
