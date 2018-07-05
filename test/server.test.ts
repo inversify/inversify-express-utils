9b025848c054dfde0a252a00c503313f26cb3b51
Zach Abney <zach.abney192@gmail.com>
2018-07-05 17:47:48 -0500

    Fix bug handling HttpResponseMessage that has no content

--- test/server.test.ts
+++ test/server.test.ts
@@ -6,6 +6,8 @@ import { controller } from "../src/decorators";
 import { Container, injectable } from "inversify";
 import { TYPE } from "../src/constants";
 import { cleanUpMetadata } from "../src/utils";
+import { HttpResponseMessage } from "../src";
+import { Mock, Times, MockBehavior } from "moq.ts";
 
 describe("Unit Test: InversifyExpressServer", () => {
 
@@ -96,5 +98,17 @@ describe("Unit Test: InversifyExpressServer", () => {
         let serverWithCustomApp = new InversifyExpressServer(container, null, null, app);
         expect((serverWithCustomApp as any)._app).to.eq(app);
         expect((serverWithDefaultApp as any)._app).to.not.eql((serverWithCustomApp as any)._app);
+    });
+
+    it("Should handle a HttpResponseMessage that has no content", () => {
+        let container = new Container();
+        let server = new InversifyExpressServer(container);
+
+        let httpResponseMessageWithoutContent = new HttpResponseMessage(404);
+        let mockResponse = new Mock<express.Response>().setBehaviorStrategy(MockBehavior.Loose);
+
+        (server as any).handleHttpResponseMessage(httpResponseMessageWithoutContent, mockResponse.object());
+
+        mockResponse.verify(instance => instance.sendStatus(404), Times.Once());
     });
 });
