b6ca0e3dcdc421da30569799d892ded01e59f0fa
Podaru Dragos <podaru.dragos@gmail.com>
2023-11-25 14:36:40 +0200

    stylelistic changes

--- src/results/StreamResult.ts
+++ src/results/StreamResult.ts
@@ -5,19 +5,18 @@ import { StreamContent } from '../content/streamContent';
 
 
 export class StreamResult implements IHttpActionResult {
-    constructor(
-        public readableStream: Readable,
-        public contentType: string,
-        public readonly statusCode: number,
-    ) {
-    }
+  constructor(
+    public readableStream: Readable,
+    public contentType: string,
+    public readonly statusCode: number
+  ) { }
 
-    public async executeAsync(): Promise<HttpResponseMessage> {
-        const response = new HttpResponseMessage(this.statusCode);
-        response.content = new StreamContent(
-            this.readableStream,
-            this.contentType,
-        );
-        return Promise.resolve(response);
-    }
+  public async executeAsync(): Promise<HttpResponseMessage> {
+    const response = new HttpResponseMessage(this.statusCode);
+    response.content = new StreamContent(
+      this.readableStream,
+      this.contentType,
+    );
+    return Promise.resolve(response);
+  }
 }
--- test/content/streamContent.test.ts
+++ test/content/streamContent.test.ts
@@ -2,44 +2,44 @@ import { Readable, Writable } from 'stream';
 import { StreamContent } from '../../src/content/streamContent';
 
 describe('StreamContent', () => {
-    it('should have text/plain as the set media type', () => {
-        const stream = new Readable();
+  it('should have text/plain as the set media type', () => {
+    const stream = new Readable();
 
-        const content = new StreamContent(stream, 'text/plain');
+    const content = new StreamContent(stream, 'text/plain');
 
-        expect(content.headers['content-type']).toEqual('text/plain');
+    expect(content.headers['content-type']).toEqual('text/plain');
+  });
+
+  it('should be able to pipe stream which was given to it', done => {
+    const stream = new Readable({
+      read() {
+        this.push(Buffer.from('test'));
+        this.push(null);
+      },
     });
 
-    it('should be able to pipe stream which was given to it', done => {
-        const stream = new Readable({
-            read() {
-                this.push(Buffer.from('test'));
-                this.push(null);
-            },
-        });
+    const content = new StreamContent(stream, 'text/plain');
 
-        const content = new StreamContent(stream, 'text/plain');
+    void content.readAsync().then((readable: Readable) => {
+      const chunks: Array<Buffer> = [];
 
-        void content.readAsync().then((readable: Readable) => {
-            const chunks: Array<Buffer> = [];
+      let buffer: Buffer | null = null;
 
-            let buffer: Buffer | null = null;
+      readable.on('end', () => {
+        buffer = Buffer.concat(chunks);
 
-            readable.on('end', () => {
-                buffer = Buffer.concat(chunks);
+        expect(buffer.toString()).toEqual('test');
 
-                expect(buffer.toString()).toEqual('test');
+        done();
+      });
 
-                done();
-            });
+      const writableStream = new Writable({
+        write(chunk) {
+          chunks.push(chunk as Buffer);
+        },
+      });
 
-            const writableStream = new Writable({
-                write(chunk) {
-                    chunks.push(chunk as Buffer);
-                },
-            });
-
-            readable.pipe(writableStream);
-        });
+      readable.pipe(writableStream);
     });
+  });
 });
